abb6af3f9820cc0d6a1089434e5476feaacb81b6
broxxer <broxxer@broxxer.de>
2017-07-05 22:20:44 +0200

    Works...

--- app/Http/Controllers/Reports/DealsPerMonthController.php
+++ app/Http/Controllers/Reports/DealsPerMonthController.php
@@ -2,10 +2,48 @@
 
 namespace App\Http\Controllers\Reports;
 
+use App\Models\DtMaster;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class DealsPerMonthController extends Controller
 {
-    //
+    public function index($year = 0, $month = 0){
+        if($year == 0){
+            $year = Carbon::now()->year;
+        }
+        if($month == 0){
+            $month = Carbon::now()->month;
+        }
+
+        $date_start = Carbon::parse($year.'-'.$month.'-01');
+        $date_end = Carbon::parse($year.'-'.$month.'-31');
+
+
+        $deals = DtMaster::where('create_date', '>=', $date_start)
+            ->where('create_date', '<=',  $date_end)
+            ->orderBy('create_date', 'desc')
+            ->paginate(20);
+
+        //Füllen des Statusarrays
+        $status = \DB::table('dt_status')->get();
+        $status_return = array();
+        foreach ($status as $s) {
+            $status_return[$s->id] = $s->value;
+        }
+
+        //Füllen der Gewinnchance
+        $cow = \DB::table('dt_chanceofwin')->get();
+        $cow_return = array();
+        foreach ($cow as $c) {
+            $cow_return[$c->id] = $c->value;
+        }
+
+        return view('deals.index', [
+            'deals'       => $deals,
+            'status'      => $status_return,
+            'chanceofwin' => $cow_return,
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -30,63 +30,8 @@ Route::get('/deal/{deal_id}', 'DealController@show');
 
 //Report Routes
 Route::get('reports', 'Reports\CockpitController@index');
+Route::get('reports/deals_per_month/{year?}/{month?}', 'Reports\DealsPerMonthController@index');
 
-
-Route::get('/charts', function()
-{
-    return View::make('mcharts');
-});
-
-Route::get('/tables', function()
-{
-    return View::make('table');
-});
-
-Route::get('/forms', function()
-{
-    return View::make('form');
-});
-
-Route::get('/grid', function()
-{
-    return View::make('grid');
-});
-
-Route::get('/buttons', function()
-{
-    return View::make('buttons');
-});
-
-
-Route::get('/icons', function()
-{
-    return View::make('icons');
-});
-
-Route::get('/panels', function()
-{
-    return View::make('panel');
-});
-
-Route::get('/typography', function()
-{
-    return View::make('typography');
-});
-
-Route::get('/notifications', function()
-{
-    return View::make('notifications');
-});
-
-Route::get('/blank', function()
-{
-    return View::make('blank');
-});
-
-Route::get('/login', function()
-{
-    return View::make('login');
-});
 
 Route::get('/documentation', function()
 {
