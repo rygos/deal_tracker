79647ae4faa31d2c37083cf800d68dff785760e6
broxxer <broxxer@broxxer.de>
2017-07-05 14:41:33 +0200

    add cockpit

--- app/Http/Controllers/Reports/CockpitController.php
+++ app/Http/Controllers/Reports/CockpitController.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers\Reports;
 
+use App\Models\DtMaster;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class CockpitController extends Controller
 {
     public function index(){
+        $master = DtMaster::get();
+        $start = Carbon::parse('first day of this month');
+        $end = Carbon::parse('last day of this month');
 
+        $data = array();
+        for($i = 12; $i >= 1; $i--){
+            $t['month_start'] = Carbon::parse('first day of this month')->addMonth(-$i+1)->toDateTimeString();
+            $t['month_end'] = Carbon::parse('last day of this month')->addMonth(-$i+1)->toDateTimeString();
+            $t['tnd'] = $master->where('create_date', '>=', $t['month_start'])->where('create_date', '<=',  $t['month_end'])->count();
+            $t['dv'] = $master->where('create_date', '>=', $t['month_start'])->where('create_date', '<=',  $t['month_end'])->Sum('revenue');
+            $t['v'] = $master->where('create_date', '>=', $t['month_start'])->where('create_date', '<=',  $t['month_end'])->Sum('volume');
+            $t['dtccpack'] = $master->where('create_date', '>=', $t['month_start'])->where('create_date', '<=',  $t['month_end'])->where('dealtype', '=', 1)->count();
+            $t['dtccplus'] = $master->where('create_date', '>=', $t['month_start'])->where('create_date', '<=',  $t['month_end'])->where('dealtype', '=', 2)->count();
+            $t['dtcomplex'] = $master->where('create_date', '>=', $t['month_start'])->where('create_date', '<=',  $t['month_end'])->whereNotIn('dealtype', [1,2])->count();
+            $data[$i-1] = $t;
+        }
+
+        return view('reports.cockpit.index', [
+            'data' => $data,
+            'start' => $start,
+            'end' => $end,
+        ]);
     }
 }
--- resources/views/reports/cockpit/index.blade.php
+++ resources/views/reports/cockpit/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.dashboard')
-@section('page_heading','Deals Übersicht')
+@section('page_heading','Cockpit')
 @section('section')
 
     <div class="col-sm-12">
@@ -8,12 +8,68 @@
                 <table class="table table-condensed table-bordered table-striped">
                     <thead>
                     <tr>
-                        <th>
-
-                        </th>
+                        <th></th>
+                        @for($i = 11; $i >= 0; $i--)
+                            <th>
+                                {{ date("M. Y", strtotime( date( 'Y-m-01' )." -$i months")) }}
+                            </th>
+                        @endfor
                     </tr>
                     </thead>
+                    <tbody>
+                    <tr>
+                        <td>Total number deals</td>
+                        @for($i = 11; $i >= 0; $i--)
+                            <td>
+                                {{ $data[$i]['tnd'] }}
+                            </td>
+                        @endfor
+                    </tr>
+                    <tr>
+                        <td>Deal Values</td>
+                        @for($i = 11; $i >= 0; $i--)
+                            <td>
+                                {{ number_format($data[$i]['dv'], 0, ',', '.') }} €
+                            </td>
+                        @endfor
+                    </tr>
+                    <tr>
+                        <td>Volume</td>
+                        @for($i = 11; $i >= 0; $i--)
+                            <td>
+                                {{ number_format($data[$i]['v'], 0, ',', '.') }}
+                            </td>
+                        @endfor
+                    </tr>
+                    <td bgcolor="#FFFFFF" style="line-height:10px;" colspan=13>&nbsp;</td>
+                    <tr>
+                        <td>Dealtype CC-Pack</td>
+                        @for($i = 11; $i >= 0; $i--)
+                            <td>
+                                {{ number_format($data[$i]['dtccpack'], 0, ',', '.') }}
+                            </td>
+                        @endfor
+                    </tr>
+                    <tr>
+                        <td>Dealtype CC-Plus</td>
+                        @for($i = 11; $i >= 0; $i--)
+                            <td>
+                                {{ number_format($data[$i]['dtccplus'], 0, ',', '.') }}
+                            </td>
+                        @endfor
+                    </tr>
+                    <tr>
+                        <td>Dealtype Complex</td>
+                        @for($i = 11; $i >= 0; $i--)
+                            <td>
+                                {{ number_format($data[$i]['dtcomplex'], 0, ',', '.') }}
+                            </td>
+                        @endfor
+                    </tr>
+                    </tbody>
                 </table>
             </div>
         </div>
     </div>
+
+@endsection
