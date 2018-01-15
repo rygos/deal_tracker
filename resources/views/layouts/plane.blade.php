4d5177353436f0560ed7cfe2ad08ef0da1599860
rygos <rygos@icloud.com>
2018-01-15 11:41:50 +0100

    test

--- resources/views/layouts/plane.blade.php
+++ resources/views/layouts/plane.blade.php
@@ -35,6 +35,7 @@
                     borderColor: '#ff6384',
                     borderWidth: 1,
                     data: [
+                        @foreach($charts['index_dpm'])
                         randomScalingFactor(),
                         randomScalingFactor(),
                         randomScalingFactor(),
@@ -42,23 +43,9 @@
                         randomScalingFactor(),
                         randomScalingFactor(),
                         randomScalingFactor()
-                    ]
-                }, {
-                    label: 'Dataset 2',
-                    backgroundColor: '#36a2eb',
-                    borderColor: '#36a2eb',
-                    borderWidth: 1,
-                    data: [
-                        randomScalingFactor(),
-                        randomScalingFactor(),
-                        randomScalingFactor(),
-                        randomScalingFactor(),
-                        randomScalingFactor(),
-                        randomScalingFactor(),
-                        randomScalingFactor()
+						@endforeach
                     ]
                 }]
-
             };
 
             window.onload = function() {
@@ -80,65 +67,6 @@
 
             };
 
-            document.getElementById('randomizeData').addEventListener('click', function() {
-                var zero = Math.random() < 0.2 ? true : false;
-                barChartData.datasets.forEach(function(dataset) {
-                    dataset.data = dataset.data.map(function() {
-                        return zero ? 0.0 : randomScalingFactor();
-                    });
-
-                });
-                window.myBar.update();
-            });
-
-            var colorNames = Object.keys(window.chartColors);
-            document.getElementById('addDataset').addEventListener('click', function() {
-                var colorName = colorNames[barChartData.datasets.length % colorNames.length];;
-                var dsColor = window.chartColors[colorName];
-                var newDataset = {
-                    label: 'Dataset ' + barChartData.datasets.length,
-                    backgroundColor: color(dsColor).alpha(0.5).rgbString(),
-                    borderColor: dsColor,
-                    borderWidth: 1,
-                    data: []
-                };
-
-                for (var index = 0; index < barChartData.labels.length; ++index) {
-                    newDataset.data.push(randomScalingFactor());
-                }
-
-                barChartData.datasets.push(newDataset);
-                window.myBar.update();
-            });
-
-            document.getElementById('addData').addEventListener('click', function() {
-                if (barChartData.datasets.length > 0) {
-                    var month = MONTHS[barChartData.labels.length % MONTHS.length];
-                    barChartData.labels.push(month);
-
-                    for (var index = 0; index < barChartData.datasets.length; ++index) {
-                        //window.myBar.addData(randomScalingFactor(), index);
-                        barChartData.datasets[index].data.push(randomScalingFactor());
-                    }
-
-                    window.myBar.update();
-                }
-            });
-
-            document.getElementById('removeDataset').addEventListener('click', function() {
-                barChartData.datasets.splice(0, 1);
-                window.myBar.update();
-            });
-
-            document.getElementById('removeData').addEventListener('click', function() {
-                barChartData.labels.splice(-1, 1); // remove the label first
-
-                barChartData.datasets.forEach(function(dataset, datasetIndex) {
-                    dataset.data.pop();
-                });
-
-                window.myBar.update();
-            });
 		</script>
 	@endif
 </body>
